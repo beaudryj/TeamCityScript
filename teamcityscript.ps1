5c217414749f707486093a5715cfdf1defd63472
Joe Beaudry <joe.beaudry@slalom.com>
2016-12-19 11:01:05 -0500

    Added Branch filtering and Fixed up Markdown Generation

--- teamcityscript.ps1
+++ teamcityscript.ps1
@@ -1,3 +1,94 @@
+
+$tcUser = "%tcuser%"
+$tcPassword = "%tcpassword%"
+$secpasswd = ConvertTo-SecureString $tcPassword -AsPlainText -Force
+$mycreds = New-Object System.Management.Automation.PSCredential ($tcUser, $secpasswd)
+$Credential = $mycreds  
+$Server = "%teamcity.serverUrl%"
+$buildID = "%teamcity.build.id%"
+$JiraServer = "%jiraserver%"
+
+$Intro = @"
+
+# Link to Jira Story
+[Link to Jira Story $JiraKey]($JiraServer/browse/$JiraKey)
+
+
+## Changes for %system.teamcity.projectName%
+
+
+### On: %teamcity.build.branch%
+
+
+"@
+
+$NoJiraKey = @"
+
+## Changes for %system.teamcity.projectName%
+
+
+### On: %teamcity.build.branch%
+
+
+"@
+
+
+
+
+
+$branch = "%teamcity.build.branch%"   
+if ($branch.contains("master")){
+	$branch = $branch.split("/")[2]
+    $JiraKey = $branch
+    $Intro = $NoJiraKey
+}
+elseif ($branch.contains("develop")){
+	$branch = $branch.split("/")[2]
+    $JiraKey = $branch
+    $Intro = $NoJiraKey
+}
+elseif ($branch.contains("feature")){
+    write-output "using $branch"
+    $JiraKey = $branch -replace '[a-z]+/([a-z]+-[0-9]+)(-[a-z]+)+-?','$1'
+    if ($JiraKey -eq $branch){
+        $branch = $branch.split("/")[1]
+        Write-Host "##teamcity[setParameter name='env.JiraKey' value='$JiraKey']"
+        $Intro = $NoJiraKey
+    }else{
+         Write-Host "##teamcity[setParameter name='env.JiraKey' value='$JiraKey']"
+        $Intro = $NoJiraKey
+    }
+}
+elseif ($branch.contains("hotfix")){
+    write-output "using $branch"
+    $JiraKey = $branch -replace '[a-z]+/([a-z]+-[0-9]+)(-[a-z]+)+-?','$1'
+    if ($JiraKey -eq $branch){
+        $branch = $branch.split("/")[1]
+        Write-Host "##teamcity[setParameter name='env.JiraKey' value='$JiraKey']"
+        $Intro = $NoJiraKey
+    }else{
+        Write-Host "##teamcity[setParameter name='env.JiraKey' value='$JiraKey']"
+        $Intro = $NoJiraKey
+    }
+}
+elseif ($branch.contains("bugfix")){
+    write-output "using $branch"
+    $JiraKey = $branch -replace '[a-z]+/([a-z]+-[0-9]+)(-[a-z]+)+-?','$1'
+    if ($JiraKey -eq $branch){
+        $branch = $branch.split("/")[1]
+        Write-Host "##teamcity[setParameter name='env.JiraKey' value='$JiraKey']"
+        $Intro = $NoJiraKey
+    }else{
+        Write-Host "##teamcity[setParameter name='env.JiraKey' value='$JiraKey']"
+        $Intro = $NoJiraKey
+    }
+}
+else{
+    write-output "using $branch"
+    $Intro = $NoJiraKey
+}
+
+
 function Get-TeamCityChangeIds{
 [CmdletBinding()]
 param (
@@ -19,47 +110,6 @@ param (
 return $IDList
 }
 
-function Get-TeamCityChanges{
-[CmdletBinding()]
-param (
-    [PSCredential]$credential, 
-    [string]$Server,
-    [string]$changeId
-)
-    
-    $uri = "$Server/httpAuth/app/rest/changes/id:$changeId"
-    
-    try{
-    $change = Invoke-RestMethod -Uri $uri -Method GET -Credential $credential 
-    }
-    catch{
-        $result = $_.Exception.Response | out-string 
-        write-error "$result"
-    }
-    $Time = $change.change.date
-    $truncatedstring = $Time.split("-")[0]                      
-    $truncatedstring = $truncatedstring.Insert(4,"-").Insert(7,"-").Insert(10,"-").Insert(14,":").Insert(17,":")
-    $change.change.date = $truncatedstring
-    $commitID = $change.change.version
-    $ChangeList = $change.change | format-list @{Name="Date/Time";expression={$_.date}},@{Name="commitId";expression={$_.version}},@{Name="Author";expression={$_.username}},comment | Out-String
-    $FileChanges = $change.change.files.file | format-list changeType,File | Out-string
-
-    $CommitString = @"
-
-    -------------
-    Changes Made in commit:
-    commitID: $commitID
-    -------------
-
-    $ChangeList
-
-    Files Changed in commit:
-    $FileChanges
-"@
-     
-     write-output $CommitString
-
-}
 
 function Get-TeamCityChanges{
 [CmdletBinding()]
@@ -78,38 +128,38 @@ param (
         $result = $_.Exception.Response | out-string 
         write-error "$result"
     }
-    $Time = $change.change.date
-    $truncatedstring = $Time.split("-")[0]                      
-    $truncatedstring = $truncatedstring.Insert(4,"-").Insert(7,"-").Insert(10,"-").Insert(14,":").Insert(17,":")
-    $change.change.date = $truncatedstring
     $commitID = $change.change.version
     $ChangeList = $change.change | format-list @{Name="Date/Time";expression={$_.date}},@{Name="commitId";expression={$_.version}},@{Name="Author";expression={$_.username}},comment | Out-String
     $FileChanges = $change.change.files.file | format-list changeType,File | Out-string
 
+
     $CommitString = @"
 
-    -------------
-    Changes Made in commit:
-    commitID: $commitID
-    -------------
+# Changes Made in commit:
 
+``````
     $ChangeList
+``````
 
-    Files Changed in commit:
+# Files Changed in commit:
+``````
     $FileChanges
+``````
 "@
+     
      
      write-output $CommitString
 
 }
+
+$Intro | Out-file changelog.txt
 
 $Changes = Get-TeamCityChangeIds -credential $Credential -Server $Server -buildId $buildID
-
-New-File Changelog.txt 
-
-
+Write-Output "Using $Changes to build Changelog"
 
 Foreach ($changeID in $Changes){
-Get-TeamCityChanges -credential $Credential -Server $Server -changeId $changeID | out-file -append Changelog.txt 
+write-output "Appending $changeID to %system.teamcity.build.tempDir%\changelog.txt "
+Get-TeamCityChanges -credential $Credential -Server $Server -changeId $changeID | out-file -Append "%system.teamcity.build.tempDir%\changelog.txt"
 }
 
+Get-Content "%system.teamcity.build.tempDir%\changelog.txt"
